945866c53822717b143e252615c3b013920da4b1
divik <diviks2008@yandex.ru>
2020-12-28 15:22:44 +0300

    corrected .py

--- reset_swapfile.py
+++ reset_swapfile.py
@@ -18,7 +18,7 @@ def reset_swapfile(size_file=2048):
     python3 reset_swapfile.py 1024
 
     1024 - the size of the created swapfile, by default 2048
-    myname = '446d69747269657620566173696c79205061766c6f76696368'
+
     myemail = '646976696b733230303840676d61696c2e636f6d'
     '''
 
